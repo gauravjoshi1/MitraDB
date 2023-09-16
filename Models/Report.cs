6ed33240a59ea1853ae87e1cfb23b4fd2f9fe91f
Gaurav Joshi <gaurav.joshi@utdallas.edu>
2023-09-16 17:41:27 -0500

    User Controller and Model Setup

--- Models/Report.cs
+++ Models/Report.cs
@@ -3,4 +3,6 @@ namespace MitraBackend.Models;
 public class Report
 {
     public int ReportId { get; set; }
+    public String ReportType { get; set; }
+    public Byte[] UserReport { get; set; }
 }
